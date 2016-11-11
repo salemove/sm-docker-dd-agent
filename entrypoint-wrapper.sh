202a856ed338e6f6bd8807819c649f1813023854
Tarvi Pillessaar <tarvi@salemove.com>
2016-11-11 11:27:16 +0200

    Histograms are disabled in last version

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -9,8 +9,4 @@ if [[ $RABBITMQ_PASS ]]; then
     sed -i -e "s/rabbitmq_pass:.*$/rabbitmq_pass: ${RABBITMQ_PASS}/" /etc/dd-agent/conf.d/auto_conf/rabbitmq.yaml
 fi
 
-# Make sure histograms are disabled, so far they have been disabled in kubernetes
-# Also they are disabled in master but not in last release
-sed -rie 's/^(\s+)\suse_histogram:/\1# use_histogram:/' /etc/dd-agent/conf.d/kubernetes.yaml.example
-
 exec /entrypoint.sh "$@"
