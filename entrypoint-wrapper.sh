95945984d849c4864479bb0f94a1867cd2439f5a
Tarvi Pillessaar <tarvi@salemove.com>
2017-05-08 11:56:09 +0300

    Disabling trace agent is broken in docker image
    Disabled it in wrapper script

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -9,4 +9,8 @@ if [[ $RABBITMQ_PASS ]]; then
     sed -i -e "s/rabbitmq_pass:.*$/rabbitmq_pass: ${RABBITMQ_PASS}/" /etc/dd-agent/conf.d/auto_conf/rabbitmq.yaml
 fi
 
+if [[ ! $DD_APM_ENABLED ]]; then
+    sed -ie 's/#\?\s*apm_enabled.*$/apm_enabled: false/' /etc/dd-agent/datadog.conf
+fi
+
 exec /entrypoint.sh "$@"
